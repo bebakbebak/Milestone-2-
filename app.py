dd37a62f9cf7f79ddf35d25f59dc77a4dc291c4f
bebakbebak <S4246251@student.rmit.edu.au>
2026-05-26 14:14:33 +1000

    Completed Flask project setup

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/hazards")
+def hazards():
+    return render_template("hazards.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
-    
